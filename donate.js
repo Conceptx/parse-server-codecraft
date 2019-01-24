cd54b6e94102fb6ebd556bf019c152749047b992
Conceptx <theophilusokoye6@gmail.com>
2019-01-24 08:52:47 +0200

    complete donation

--- donate.js
+++ donate.js
@@ -10,26 +10,23 @@ router.post('/', (req, res) => {
     .set('X-Parse-Application-Id', `${process.env.APP_ID}`)
     .type('json')
     .send(req.body)
-    .then(async response => {
+    .then(response => {
       // if (response.body.code) {
       //   console.log(response.body.error);
       //   return;
       // }
       if (req.body.paymentMethod === 'paynow') {
-        const paynow = await new Paynow(
+        const paynow = new Paynow(
           `${process.env.PAYNOWID}`,
           `${process.env.PAYNOWKEY})`,
           '',
           'https://parse-server-me.herokuapp.com/'
         );
-        const payment = await paynow.createPayment(
-          req.body.purpose,
-          req.body.email
-        );
+        const payment = paynow.createPayment(req.body.purpose, req.body.email);
         payment.add('FUNDS', req.body.amount);
-        const init = await paynow.send(payment);
+        const init = paynow.send(payment);
         const link = init.success ? init.redirectUrl : { success: false };
-        return link;
+        return res.redirect(link);
       } else if (req.body.paymentMethod === 'paypal') {
         return res.json({ success: true });
       }
