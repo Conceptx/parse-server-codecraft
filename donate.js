9aeffc251cfb5be9165cd9c25ec64f6883fd5c7f
Conceptx <theophilusokoye6@gmail.com>
2019-01-24 09:33:07 +0200

    paynow fix

--- donate.js
+++ donate.js
@@ -3,6 +3,13 @@ const router = express.Router();
 const request = require('superagent');
 const Paynow = require('paynow');
 
+const paynow = new Paynow(
+  `${process.env.PAYNOWID}`,
+  `${process.env.PAYNOWKEY})`,
+  'https://parse-server-me.herokuapp.com/',
+  'https://parse-server-me.herokuapp.com/'
+);
+
 router.post('/', (req, res) => {
   request
     .post('https://parse-server-me.herokuapp.com/parse/classes/donations')
@@ -10,19 +17,13 @@ router.post('/', (req, res) => {
     .set('X-Parse-Application-Id', `${process.env.APP_ID}`)
     .type('json')
     .send(req.body)
-    .then(async () => {
+    .then(async response => {
       // if (response.body.code) {
       //   console.log(response.body.error);
       //   return;
       // }
       console.log('Response: ' + response.body);
       if (req.body.paymentMethod === 'paynow') {
-        const paynow = new Paynow(
-          `${process.env.PAYNOWID}`,
-          `${process.env.PAYNOWKEY})`,
-          'https://parse-server-me.herokuapp.com/',
-          'https://parse-server-me.herokuapp.com/'
-        );
         console.log('Paynow: ' + paynow);
         const payment = await paynow.createPayment(
           req.body.purpose,
